7ee656e8d62e90c05d9684e3a0ca7bed624a6741
JacobMorga <jacobam0307@gmail.com>
2024-08-19 21:49:16 -0700

    Update robotConfig.cpp

--- HighStakes1/src/robotConfig.cpp
+++ HighStakes1/src/robotConfig.cpp
@@ -2,23 +2,23 @@
 
 Controller controller (CONTROLLER_MASTER);
 
-Motor drive1 (16, MOTOR_GEAR_600, true, MOTOR_ENCODER_DEGREES);
-Motor drive2 (15, MOTOR_GEAR_600, false, MOTOR_ENCODER_DEGREES);
-Motor drive3 (7, MOTOR_GEAR_600, true, MOTOR_ENCODER_DEGREES);
+Motor drive1 (1, MOTOR_GEAR_600, true, MOTOR_ENCODER_DEGREES);
+Motor drive2 (2, MOTOR_GEAR_600, false, MOTOR_ENCODER_DEGREES);
+Motor drive3 (3, MOTOR_GEAR_600, true, MOTOR_ENCODER_DEGREES);
 Motor_Group rightDrive ({drive1,drive2,drive3});
 
-Motor drive4 (20, MOTOR_GEAR_600, false, MOTOR_ENCODER_DEGREES);
-Motor drive5 (18, MOTOR_GEAR_600, true, MOTOR_ENCODER_DEGREES);
-Motor drive6 (17, MOTOR_GEAR_600, false, MOTOR_ENCODER_DEGREES);
+Motor drive4 (11, MOTOR_GEAR_600, false, MOTOR_ENCODER_DEGREES);
+Motor drive5 (12, MOTOR_GEAR_600, true, MOTOR_ENCODER_DEGREES);
+Motor drive6 (14, MOTOR_GEAR_600, false, MOTOR_ENCODER_DEGREES);
 Motor_Group leftDrive ({drive4,drive5,drive6});
 
 Motor_Group drivetrain ({drive1,drive2,drive3, drive4,drive5,drive6});
 
 Motor intake (4, MOTOR_GEAR_600, false, MOTOR_ENCODER_DEGREES);
 
-Rotation xTracking (11);
-Rotation yTracking (12);
+Rotation xTracking (5);
+Rotation yTracking (6);
 
-IMU inertial1 (2);
-IMU inertial2 (13);
-IMU inertial3 (14);
+IMU inertial1 (7);
+IMU inertial2 (8);
+IMU inertial3 (9);
