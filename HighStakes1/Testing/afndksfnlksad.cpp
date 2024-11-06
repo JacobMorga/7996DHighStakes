5accae1b62169560b4cabb9339d9bde03424f78d
JacobMorga <jacobam0307@gmail.com>
2024-11-06 00:07:59 -0700

    Added circle code to main file and optimized

--- HighStakes1/Testing/afndksfnlksad.cpp
+++ HighStakes1/Testing/afndksfnlksad.cpp
@@ -86,20 +86,14 @@ void funchinfunction (){
 vector<vector<float>> path {{-2.8, 2.9},{-1.4, -3.3},{-0.59,3.77},{3.0,-5.0},{3.0,4.01}};
 vector<vector<float>> intersectionPoints {};
 
-
-
 float px1 = 0.0;
 float py1 = 0.0;
 float px2 = 0.0;
 float py2 = 0.0;
 
 int counter = 0;
-
-float currentX = 0.0;
-float currentY = 0.0;
-double lookAheadRadius = 3.0;
     
-void funchininerfunction (vector<vector<float>> straightLinePath, float robotX, float robotY, float lookAheadDis){
+vector<vector<float>> circlePathIntersection (vector<vector<float>> straightLinePath, float robotX, float robotY, float lookAheadDis){
 
     // move the robot center to the origin (easier math)
     for (vector<float> point : straightLinePath){
@@ -139,9 +133,9 @@ void funchininerfunction (vector<vector<float>> straightLinePath, float robotX, 
             intY2 = (-diffD * diffX - fabs(diffY) * sqrtf(powf(lookAheadDis, 2.0) * powf(diffR, 2.0) - powf(diffD, 2.0))) / powf(diffR, 2.0);
 
 
-            if (intX1 == intX2 && intY1 == intY2){
+            if (intX1 == intX2 && intY1 == intY2){ // Two intersections are the same
 
-                if ((intX1 > px1 && intX1 > px2) || (intX1 < px1 && intX1 < px2)){
+                if (!(intX1 > px1 && intX1 > px2) && !(intX1 < px1 && intX1 < px2)){
                     cout << "point 1 failed" << "\n";
                 }
                 else {
@@ -150,22 +144,24 @@ void funchininerfunction (vector<vector<float>> straightLinePath, float robotX, 
                 }
             }
             else {
-                if ((intX1 > px1 && intX1 > px2) || (intX1 < px1 && intX1 < px2)){
+                if (!(intX1 > px1 && intX1 > px2) && !(intX1 < px1 && intX1 < px2)){
+                    intersectionPoints.push_back({intX1, intY1});
+                }
+                else {
+
                     cout << "point 1 failed" << "\n";
                     cout << "intersection X1 = " << intX1 << "   " << px1 << "   " << "\n";
                     cout << "intersection Y1 = " << intY1 << "   " << px2 << "   " << "\n";
                 }
-                else {
-                    intersectionPoints.push_back({intX1, intY1});
-                }
 
-                if ((intX2 > px1 && intX2 > px2) || (intX2 < px1 && intX2 < px2)){
-                    cout << "point 2 failed" << "\n";
+                if (!(intX2 > px1 && intX2 > px2) && !(intX2 < px1 && intX2 < px2)){
+                    intersectionPoints.push_back({intX2, intY2});
+                }
+                else {
+                    
+                    cout << "point 2 failed" << "\n"; 
                     cout << "intersection X2 = " << intX2 << "   " << px1 << "   " << "\n";
                     cout << "intersection Y2 = " << intY2 << "   " << px2 << "   " << "\n";
-                }
-                else {
-                    intersectionPoints.push_back({intX2, intY2});
                 }
             }
         }
@@ -182,12 +178,14 @@ void funchininerfunction (vector<vector<float>> straightLinePath, float robotX, 
         }
         cout << "\n";
     }
+
+    return intersectionPoints;
 }
 
 
 int main (){
 
-    funchininerfunction(path, currentX, currentY, lookAheadRadius);
+    circlePathIntersection(path, 0.0, 0.0, 3.0);
 
     return 1;
 }
--- HighStakes1/src/motionAlgs.cpp
+++ HighStakes1/src/motionAlgs.cpp
@@ -136,6 +136,83 @@ void followArc (float xtar, float ytar, float ttar){
     
 }
 
+vector<vector<float>> path {{-2.8, 2.9},{-1.4, -3.3},{-0.59,3.77},{3.0,-5.0},{3.0,4.01}};
+vector<vector<float>> intersectionPoints {};
+
+float px1 = 0.0;
+float py1 = 0.0;
+float px2 = 0.0;
+float py2 = 0.0;
+
+float diffX = 0.0;
+float diffY = 0.0;
+float diffR = 0.0;
+float diffD = 0.0;
+
+float intersectionCount = 0.0;
+int counter = 0;
+
+float intX1 = 0.0;
+float intX2 = 0.0;
+float intY1 = 0.0;
+float intY2 = 0.0;
+    
+vector<vector<float>> circlePathIntersection (vector<vector<float>> straightLinePath, float robotX, float robotY, float lookAheadDis){
+
+    // move the robot center to the origin (easier math)
+    for (vector<float> point : straightLinePath){
+
+        point[0] -= robotX;
+        point[1] -= robotY;
+    }
+
+    while (counter < straightLinePath.size() - 1){
+
+        px1 = straightLinePath[counter][0];
+        py1 = straightLinePath[counter][1];
+        px2 = straightLinePath[counter + 1][0];
+        py2 = straightLinePath[counter + 1][1];
+
+        // begining of calculations
+        diffX = px2-px1;
+        diffY = py2-py1;
+
+        diffR = sqrtf(powf(diffX, 2.0) + powf(diffY, 2.0));
+        diffD = px1*py2 - px2*py1;
+
+        intersectionCount = powf(lookAheadDis, 2.0) * powf(diffR, 2.0) - powf(diffD, 2.0);
+
+        if (intersectionCount >= 0.0){ // One or two intersections
+
+            intX1 = (diffD * diffY + fabs(diffY) / diffY * diffX * sqrtf(powf(lookAheadDis, 2.0) * powf(diffR, 2.0) - powf(diffD, 2.0))) / powf(diffR, 2.0);
+            intX2 = (diffD * diffY - fabs(diffY) / diffY * diffX * sqrtf(powf(lookAheadDis, 2.0) * powf(diffR, 2.0) - powf(diffD, 2.0))) / powf(diffR, 2.0);
+
+            intY1 = (-diffD * diffX + fabs(diffY) * sqrtf(powf(lookAheadDis, 2.0) * powf(diffR, 2.0) - powf(diffD, 2.0))) / powf(diffR, 2.0);
+            intY2 = (-diffD * diffX - fabs(diffY) * sqrtf(powf(lookAheadDis, 2.0) * powf(diffR, 2.0) - powf(diffD, 2.0))) / powf(diffR, 2.0);
+
+
+            if (intX1 == intX2 && intY1 == intY2){
+
+                if (!(intX1 > px1 && intX1 > px2) && !(intX1 < px1 && intX1 < px2)){
+                    intersectionPoints.push_back({intX1, intY1});
+                }
+            }
+            else {
+                if (!(intX1 > px1 && intX1 > px2) && !(intX1 < px1 && intX1 < px2)){
+                    intersectionPoints.push_back({intX1, intY1});
+                }
+
+                if (!(intX2 > px1 && intX2 > px2) && !(intX2 < px1 && intX2 < px2)){
+                    intersectionPoints.push_back({intX2, intY2});
+                }
+            }
+        }
+        counter++;
+    }
+
+    return intersectionPoints;
+}
+
 void PurePursuit (){
 
 
