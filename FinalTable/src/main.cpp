1ad7c844f64aa93f9a3cd021dcc0d5451de524bc
JacobMorga <jacobam0307@gmail.com>
2025-02-28 19:22:10 -0700

    Adjust path origin to avoid robot collision at (0,0)

--- FinalTable/src/main.cpp
+++ FinalTable/src/main.cpp
@@ -113,7 +113,7 @@ void opcontrol() {
 
 	
 	//std::vector<coord> pathJohn = {coord(0.0, 0.0), coord(0.0, 96.0), coord(-96.0, 0.0), coord(-96.0, 96.0)};
-	std::vector<coord> pathJohn = {coord(0.1, 0.1), coord(0.0, 96.0), coord(-96.0,96.0), coord(-96.0,0.0)};
+	std::vector<coord> pathJohn = {coord(0.01, 0.01), coord(0.0, 96.0), coord(-96.0,96.0), coord(-96.0,0.0)};
 	doThePurePursuit(pathJohn);
 	
 	
