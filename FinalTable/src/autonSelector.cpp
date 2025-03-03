40832169867ca8f6a4e1cb898e5c644d10081a64
Carson Wengert <carson.wengert@gmail.com>
2025-03-03 14:42:34 -0700

    working on skills idk if i merged last night

--- FinalTable/src/autonSelector.cpp
+++ FinalTable/src/autonSelector.cpp
@@ -96,15 +96,15 @@ void autonSelector (){
         }
         
         screen::set_pen(COLOR_WHITE);
-        screen::print(TEXT_MEDIUM_CENTER,120 + xTxtOffset,0   + yTxtOffset, "BPos AWP");
-        screen::print(TEXT_MEDIUM_CENTER,240 + xTxtOffset,0   + yTxtOffset, "BNeg AWP");
-        screen::print(TEXT_MEDIUM_CENTER,360 + xTxtOffset,0   + yTxtOffset, "RPos AWP");
-        screen::print(TEXT_MEDIUM_CENTER,120 + xTxtOffset,80  + yTxtOffset, "RNeg AWP");
-        screen::print(TEXT_MEDIUM_CENTER,240 + xTxtOffset,80  + yTxtOffset, "BPos Max");
-        screen::print(TEXT_MEDIUM_CENTER,360 + xTxtOffset,80  + yTxtOffset, "BNeg Max");
-        screen::print(TEXT_MEDIUM_CENTER,120 + xTxtOffset,160 + yTxtOffset, "RPos Max");
-        screen::print(TEXT_MEDIUM_CENTER,240 + xTxtOffset,160 + yTxtOffset, "RNeg Max");
-        screen::print(TEXT_MEDIUM_CENTER,360 + xTxtOffset,160 + yTxtOffset, "Skills");
+        screen::print(TEXT_MEDIUM_CENTER, 120 + xTxtOffset, 0   + yTxtOffset, "BPos AWP");
+        screen::print(TEXT_MEDIUM_CENTER, 240 + xTxtOffset, 0   + yTxtOffset, "BNeg AWP");
+        screen::print(TEXT_MEDIUM_CENTER, 360 + xTxtOffset, 0   + yTxtOffset, "RPos AWP");
+        screen::print(TEXT_MEDIUM_CENTER, 120 + xTxtOffset, 80  + yTxtOffset, "RNeg AWP");
+        screen::print(TEXT_MEDIUM_CENTER, 240 + xTxtOffset, 80  + yTxtOffset, "BPos Max");
+        screen::print(TEXT_MEDIUM_CENTER, 360 + xTxtOffset, 80  + yTxtOffset, "BNeg Max");
+        screen::print(TEXT_MEDIUM_CENTER, 120 + xTxtOffset, 160 + yTxtOffset, "RPos Max");
+        screen::print(TEXT_MEDIUM_CENTER, 240 + xTxtOffset, 160 + yTxtOffset, "RNeg Max");
+        screen::print(TEXT_MEDIUM_CENTER, 360 + xTxtOffset, 160 + yTxtOffset, "Skills");
 
                                if (teamColor == COLOR_RED){
 		std::cout << "RED";
