8e4aaa2e403b97c2eb911b701bf9432980afbe83
JacobMorga <jacobam0307@gmail.com>
2025-03-05 12:23:24 -0700

    New 4x4 auton selector

--- FinalTable/src/autonSelector.cpp
+++ FinalTable/src/autonSelector.cpp
@@ -10,8 +10,8 @@ screen_touch_status_s_t status;
 
 int row, column = 0;
 int autonSelected = 0;
-int xTxtOffset = 30;
-int yTxtOffset = 30;
+int xTxtOffset = 20;
+int yTxtOffset = 20;
 
 void autonSelector (){
 
@@ -23,120 +23,100 @@ void autonSelector (){
 
         status = screen::touch_status();
 
-                       if (teamColor == COLOR_RED){
-		std::cout << "RED";
-	}
-	else if (teamColor == COLOR_BLUE){
-		std::cout << "BLUE";
-	}
-	else{
-		std::cout << "NO COLOR";
-	}
-
-	std::cout << " : " << autonSelected << "\n";
-
-        screen::set_pen(COLOR_WHITE);
-        screen::set_eraser(COLOR_BLACK);
-
-        if (teamColor == COLOR_RED){
-            screen::erase_rect(0,0,119,240);
-            delay(20);
-            screen::set_pen(teamColor);
-            screen::fill_rect(0,0,119,119);
             screen::set_pen(COLOR_WHITE);
-            screen::draw_rect(0,120,119,240);
+            screen::set_eraser(COLOR_BLACK);
+/*
+            if (teamColor == COLOR_RED){
+                screen::erase_rect(0,0,119,240);
+                delay(20);
+                screen::set_pen(teamColor);
+                screen::fill_rect(0,0,119,119);
+                screen::set_pen(COLOR_WHITE);
+                screen::draw_rect(0,120,119,240);
 
-        }
-        else {
-            screen::erase_rect(0,0,119,240);
-            screen::set_pen(teamColor);
-            delay(20);
-            screen::fill_rect(0,120,119,240);
-            screen::set_pen(COLOR_WHITE);
-            screen::draw_rect(0,0,119,119);
-        }
+            }
+            else {
+                screen::erase_rect(0,0,119,240);
+                screen::set_pen(teamColor);
+                delay(20);
+                screen::fill_rect(0,120,119,240);
+                screen::set_pen(COLOR_WHITE);
+                screen::draw_rect(0,0,119,119);
+            }
+*/
+            //screen::set_pen(COLOR_WHITE);
+            screen::draw_line(240,0,240,240); // Splits auton box vertically 
+            screen::draw_line(120,0,120,240);
+            screen::draw_line(360,0,360,240);
 
-        screen::set_pen(COLOR_WHITE);
-        screen::erase_rect(120,0,480,240); // Draws main auton box
-        screen::draw_line(240,0,240,240); // Splits auton box vertically 
-        screen::draw_line(360,0,360,240);
-        screen::draw_line(120,80,480,80); // Splits auton box horizontally  
-        screen::draw_line(120,160,480,160);
-        
-        if (status.x < 120){ // Red and blue button
+            screen::draw_line(0,60,480,60);
+            screen::draw_line(0,120,480,120);
+            screen::draw_line(0,180,480,180);
+                
+            if(status.y < 60){ row = 1; }
+            else if(60 < status.y < 120){ row = 2; }
+            else if(120 < status.y < 180){ row = 3; }
+            else{ row = 4; }
 
-            if (status.y < 120){ teamColor = COLOR_RED; }
-            else { teamColor = COLOR_BLUE; }
-        }
-        else { // Auton squares
-            
-            if(status.y < 80){ row = 1; }
-            else if(status.y < 160){ row = 2; }
-            else{ row = 3; }
-
-            if(status.x > 360){ column = 3; }
-            else if(status.x > 240){ column = 2; }
-            else { column = 1; }
+            if(status.x < 120){ column = 1; }
+            else if(120 < status.x < 240){ column = 2; }
+            else if(240 < status.x < 360){ column = 3; }
+            else { column = 4; }
 
             std::cout << row << ":" << column << "\n";
 
             screen::set_pen(COLOR_GREEN);
 
-            if      (row == 1 && column == 1){ autonSelected = 1; screen::fill_rect(120,0  ,240,80 ); }
-            else if (row == 1 && column == 2){ autonSelected = 2; screen::fill_rect(240,0  ,360,80 ); }
-            else if (row == 1 && column == 3){ autonSelected = 3; screen::fill_rect(360,0  ,480,80 ); }
-            else if (row == 2 && column == 1){ autonSelected = 4; screen::fill_rect(120,80 ,240,160); }
-            else if (row == 2 && column == 2){ autonSelected = 5; screen::fill_rect(240,80 ,360,160); }
-            else if (row == 2 && column == 3){ autonSelected = 6; screen::fill_rect(360,80 ,480,160); }
-            else if (row == 3 && column == 1){ autonSelected = 7; screen::fill_rect(120,160,240,240); }
-            else if (row == 3 && column == 2){ autonSelected = 8; screen::fill_rect(240,160,360,240); }
-            else if (row == 3 && column == 3){ autonSelected = 9; screen::fill_rect(360,160,480,240); }
+            if      (row == 1 && column == 1){ autonSelected = 1;  screen::fill_rect(0  ,0  ,120,60 ); }
+            else if (row == 1 && column == 2){ autonSelected = 2;  screen::fill_rect(120,0  ,240,60 ); }
+            else if (row == 1 && column == 3){ autonSelected = 3;  screen::fill_rect(240,0  ,360,60 ); }
+            else if (row == 1 && column == 4){ autonSelected = 4;  screen::fill_rect(360,0  ,480,60 ); }
+            else if (row == 2 && column == 1){ autonSelected = 5;  screen::fill_rect(0  ,60 ,120,120); }
+            else if (row == 2 && column == 2){ autonSelected = 6;  screen::fill_rect(120,60 ,240,120); }
+            else if (row == 2 && column == 3){ autonSelected = 7;  screen::fill_rect(240,60 ,360,120); }
+            else if (row == 2 && column == 4){ autonSelected = 8;  screen::fill_rect(360,60 ,480,120); }
+            else if (row == 3 && column == 1){ autonSelected = 19; screen::fill_rect(0  ,120,120,180); }
+            else if (row == 3 && column == 2){ autonSelected = 10; screen::fill_rect(120,120,240,180); }
+            else if (row == 3 && column == 3){ autonSelected = 11; screen::fill_rect(240,120,360,180); }
+            else if (row == 3 && column == 4){ autonSelected = 12; screen::fill_rect(360,120,480,180); }
+            else if (row == 4 && column == 1){ autonSelected = 13; screen::fill_rect(0  ,180,120,240); }
+            else if (row == 4 && column == 2){ autonSelected = 14; screen::fill_rect(120,180,240,240); }
+            else if (row == 4 && column == 3){ autonSelected = 15; screen::fill_rect(240,180,360,240); }
+            else if (row == 4 && column == 4){ autonSelected = 16; screen::fill_rect(360,180,480,240); }
             else                             { autonSelected = 0; }
 
-        }
-        
-        screen::set_pen(COLOR_WHITE);
-        screen::print(TEXT_MEDIUM_CENTER, 120 + xTxtOffset, 0   + yTxtOffset, "BPos AWP");
-        screen::print(TEXT_MEDIUM_CENTER, 240 + xTxtOffset, 0   + yTxtOffset, "BNeg AWP");
-        screen::print(TEXT_MEDIUM_CENTER, 360 + xTxtOffset, 0   + yTxtOffset, "RPos AWP");
-        screen::print(TEXT_MEDIUM_CENTER, 120 + xTxtOffset, 80  + yTxtOffset, "RNeg AWP");
-        screen::print(TEXT_MEDIUM_CENTER, 240 + xTxtOffset, 80  + yTxtOffset, "BPos Max");
-        screen::print(TEXT_MEDIUM_CENTER, 360 + xTxtOffset, 80  + yTxtOffset, "BNeg Max");
-        screen::print(TEXT_MEDIUM_CENTER, 120 + xTxtOffset, 160 + yTxtOffset, "RPos Max");
-        screen::print(TEXT_MEDIUM_CENTER, 240 + xTxtOffset, 160 + yTxtOffset, "RNeg Max");
-        screen::print(TEXT_MEDIUM_CENTER, 360 + xTxtOffset, 160 + yTxtOffset, "Skills");
+            
+            screen::set_pen(COLOR_WHITE);
 
-                               if (teamColor == COLOR_RED){
-		std::cout << "RED";
-	}
-	else if (teamColor == COLOR_BLUE){
-		std::cout << "BLUE";
-	}
-	else{
-		std::cout << "NO COLOR";
-	}
-    	std::cout << " : " << autonSelected << "\n";
+            screen::print(TEXT_MEDIUM_CENTER, 0  +xTxtOffset,0  +yTxtOffset, "1"); 
+            screen::print(TEXT_MEDIUM_CENTER, 120+xTxtOffset,0  +yTxtOffset, "2"); 
+            screen::print(TEXT_MEDIUM_CENTER, 240+xTxtOffset,0  +yTxtOffset, "3"); 
+            screen::print(TEXT_MEDIUM_CENTER, 360+xTxtOffset,0  +yTxtOffset, "4"); 
+            screen::print(TEXT_MEDIUM_CENTER, 0  +xTxtOffset,60 +yTxtOffset, "5"); 
+            screen::print(TEXT_MEDIUM_CENTER, 120+xTxtOffset,60 +yTxtOffset, "6"); 
+            screen::print(TEXT_MEDIUM_CENTER, 240+xTxtOffset,60 +yTxtOffset, "7"); 
+            screen::print(TEXT_MEDIUM_CENTER, 360+xTxtOffset,60 +yTxtOffset, "8"); 
+            screen::print(TEXT_MEDIUM_CENTER, 0  +xTxtOffset,120+yTxtOffset, "9"); 
+            screen::print(TEXT_MEDIUM_CENTER, 120+xTxtOffset,120+yTxtOffset, "10"); 
+            screen::print(TEXT_MEDIUM_CENTER, 240+xTxtOffset,120+yTxtOffset, "11"); 
+            screen::print(TEXT_MEDIUM_CENTER, 360+xTxtOffset,120+yTxtOffset, "12"); 
+            screen::print(TEXT_MEDIUM_CENTER, 0  +xTxtOffset,180+yTxtOffset, "13"); 
+            screen::print(TEXT_MEDIUM_CENTER, 120+xTxtOffset,180+yTxtOffset, "14"); 
+            screen::print(TEXT_MEDIUM_CENTER, 240+xTxtOffset,180+yTxtOffset, "15"); 
+            screen::print(TEXT_MEDIUM_CENTER, 360+xTxtOffset,180+yTxtOffset, "16"); 
 
-        while(status.touch_status == E_TOUCH_HELD){
-            status = pros::screen::touch_status();
-            delay(20);
-        }
-        while(status.touch_status != E_TOUCH_HELD){
-            status = pros::screen::touch_status();
-            delay(20);
-        }
 
-        if (status.x < 120){ // Red and blue button
-
-            if (status.y < 120){ teamColor = COLOR_RED; }
-            else { teamColor = COLOR_BLUE; }
-        }
+            while(status.touch_status == E_TOUCH_HELD){
+                status = pros::screen::touch_status();
+                delay(20);
+            }
+            while(status.touch_status != E_TOUCH_HELD){
+                status = pros::screen::touch_status();
+                delay(20);
+            }
 
         screen::erase();
 
         delay(20);
-
-        
     }
-
 }
