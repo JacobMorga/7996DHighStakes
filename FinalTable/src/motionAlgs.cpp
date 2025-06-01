3e2f293b21148c1e9d06f4d50e1e23df2b2d6690
JacobMorga <jacobam0307@gmail.com>
2025-06-01 11:45:27 -0700

    Finished comments for motionAlgs I think

--- FinalTable/src/motionAlgs.cpp
+++ FinalTable/src/motionAlgs.cpp
@@ -10,60 +10,78 @@
  * - toPointShortBy: Moves the robot to a point offset by a certain distance. Useful for lining up at a certain radius from an object. (High Stakes wall stakes)
  * - faceAway: Aligns the robot to face away from a specific point. (Reverse of toPoint)
  * - faceHeading: Aligns the robot to a specific heading.
+ * 
+ * All these functions use the basic theory of PIDs combined with the information from  odometry to create complex motion algorithms that allow more efficient, faster, and more complex autonomous routines
  */
 
-float linKP = 550.0;//500.0;
-float linKI = 3.0;//1.0;
-float linKD = 5000.0;//5000.0;
+// Linear PID tuning values (front to back error) (Previous & useful values stored in comments) (# = Variable expanded name)
+//? Note to future teams - these values used should not be used as "starting values" for your own PIDs, as these are just scaling vales the numbers that work for us and our robot will NOT work for you.
+float linKP = 550.0;//500.0; //# Linear Constant of Proportion
+float linKI = 3.0;//1.0; //# Linear Constant of Integral
+float linKD = 5000.0;//5000.0; //# Linear Constant of Derivative
 
-float rotKP = 25000.0; //15000.0;
-float rotKI = 1000.0; //750.0;
-float rotKD = 175000.0; //100000.0; //106250.0; 
+// Rotation PID tuning values (Yaw) (Previous & useful values stored in comments)
+float rotKP = 25000.0; //15000.0; //# Rotational Constant of Propotion
+float rotKI = 1000.0; //750.0; //# Rotational Constant of Integral
+float rotKD = 175000.0; //100000.0; //106250.0; //# Rotational Constant of Derivative
 
-const float tErrorMin = -1.0; //*tune after tuning rotKI
-const float tErrorMax = 5.0 / 180.0 * pi; //*tune after tuning rotKI
-const float tIntMax = 2000.0; //*tune after tuning rotKI
+// Exit condition tuning values
+const float tErrorMin = -1.0; //# Theta Error Minimum 
+const float tErrorMax = 5.0 / 180.0 * pi; //# Theta Error Maximum
+const float tIntMax = 2000.0; //# Theta Integration Maximum
 
-float tError = 0.0;
-float tInt = 0.0;
-float tDer = 0.0;
-float tPrevError = 0.0;
-float tPow = 0.0;
-float tTarget = 0.0; //this is a variable while tTar is an input
-int exitLoops = 0; //universal
-bool pseudoVelSwitch = 0;
-float pseudoVelLimit = 1.0;
-float pseudoRotVelLimit = 1.0;
-float maxAcceptableRotError = 1.5;
-float smoothFPError = 5.0;
-bool checkPseudoVel = 1;
+// Variable initialization
+float tError = 0.0; //# Theta Error
+float tInt = 0.0; //# Theta Intergal
+float tDer = 0.0; //# Theta Derivative
+float tPrevError = 0.0; //# Theta Previous Error
+float tPow = 0.0; //# Theta Power
+float tTarget = 0.0; //this is a variable while tTar is an input //# Theta Target
+int exitLoops = 0; //universal //# Exit Loops
+bool pseudoVelSwitch = 0; //# Pseudo Velocity Switch
+float pseudoVelLimit = 1.0; //# Pseudo Velocity Switch
+float pseudoRotVelLimit = 1.0; //# Pseudo Rotational Velocity Limit
+float maxAcceptableRotError = 1.5; //# Maximum Acceptable Rotational Error
+float smoothFPError = 5.0; //# Smooth Face Point Error
+bool checkPseudoVel = 1; //# Check Pseudo Velocity
 
+/**
+ * facePoint
+ *  Takes a (x,y) coordinate for the point to face, a boolean to activate smooth, and a maximum cap on the speed during the maneuver
+ *      xTar - (float, inches) X Position for the target point to face //# X Target
+ *      yTar - (float, inches) Y Position for the target point to face //# Y Target
+ *      fpSmooth - (boolean) 1 to activate "smooth" which exits the function early to provide faster and more fluid routines at the cost of exact precision //# Face Point Smooth
+ *      speedLimit - (float, milliVolts) Caps the maximum speed of the drive during the manuever
+ * 
+ * !Pseudo Velocity:
+ *      Pseduo velocity is a measurement created to check the robots relative speed. The variable was created as a means to check if the robot was stuck. We call it pseudo velocity because it is how far the robot moved in the last loop of odometry, similar to velocity but not quite.
+ */
 void facePoint(float xTar, float yTar, bool fpSmooth, float speedLimit){
+    // Reset variables
     pseudoVelSwitch = 0;
     exitLoops = 0;
-    while (exitLoops < 25){
-        tPos = (pi / 2.0) - tPos;
-        tTarget = arctan2(xTar - xPos, yTar - yPos);
-        tError = normAngle(tTarget - tPos);
-        tInt += tError;
-        if (fabs(tError) >= tErrorMax || fabs(tInt) >= tIntMax){tInt = 0.0;}
-        tDer = tError - tPrevError;
-        tPrevError = tError;
-        tPow = rotKP * tError + rotKI * tInt + rotKD * tDer;
-        if(fabs(tPow) > speedLimit){tPow = getDir(tPow) * speedLimit;}
-        rightDrive.move_voltage(tPow); 
+    while (exitLoops < 25){ // Run until the robot is within target for so long
+        tPos = (pi / 2.0) - tPos; // Set "0 Degrees" -> "pi/2 Radians" for easier math //! Not exactly sure how this works 
+        tTarget = arctan2(xTar - xPos, yTar - yPos); // Calculate theta target (Rotation from X-Axis, updates every loop)
+        tError = normAngle(tTarget - tPos); // Find the error between robots current heading and heading to target (Normalize to -pi to pi)
+        tInt += tError; // Update integral value
+        if (fabs(tError) >= tErrorMax || fabs(tInt) >= tIntMax){tInt = 0.0;} // Set limits on integral
+        tDer = tError - tPrevError; // Calculate theta derivative 
+        tPrevError = tError; // Update error for next loop
+        tPow = rotKP * tError + rotKI * tInt + rotKD * tDer; // Calculate power by multiply by scaling constants
+        if(fabs(tPow) > speedLimit){tPow = getDir(tPow) * speedLimit;} // Set power to power limit if above
+        rightDrive.move_voltage(tPow); // Move drive motors
         leftDrive.move_voltage(-tPow);
 
-        if(pseudoVelSwitch == 0 && fabs(pseudoRotVel) > pseudoRotVelLimit){pseudoVelSwitch = 1;}
+        if(pseudoVelSwitch == 0 && fabs(pseudoRotVel) > pseudoRotVelLimit){pseudoVelSwitch = 1;} // Prevents pseudo velocity from triggering right at start of the movement when the robot is not moving yet
 
-        if (fabs(tError / pi * 180.0) <= maxAcceptableRotError || (pseudoVelSwitch == 1 && fabs(pseudoRotVel) < pseudoRotVelLimit)){exitLoops += 1;}
-        else if(fpSmooth == 1 && fabs(tError / pi * 180.0) < smoothFPError){exitLoops += 25;}
-        else{exitLoops = 0;}
+        if (fabs(tError / pi * 180.0) <= maxAcceptableRotError || (pseudoVelSwitch == 1 && fabs(pseudoRotVel) < pseudoRotVelLimit)){exitLoops += 1;} // If the robot is within a certain amount from the target or the robot has stopped, start exiting the loop
+        else if(fpSmooth == 1 && fabs(tError / pi * 180.0) < smoothFPError){exitLoops += 25;} // Second trigger point with a much larger zone for linking movements together, exits immediatly once robot hits the target zone
+        else{exitLoops = 0;} // If doesn't meet any condition reset
 
         delay(10);
     }
-    drivetrain.brake();
-    //lcd::set_text(7, "freaky time");
+    drivetrain.brake(); // Stop drive at end of movement (motors will stay at last commanded velocity if not set to 0)
 }
 
 const float lErrorMin = 0.25; 
@@ -111,7 +129,8 @@ bool TPSB1 = 1;
 
 void toPoint(float xTar, float yTar, float reversed, bool smooth, float powerLimit){
 
-    xTar *= fieldRatio;
+    // Reset and init variables 
+    xTar *= fieldRatio; // Sets the target in field tiles lengths to inches (Field tiles are not exactly 48 inches so when we say "move forward 48 in" we want it to move one field tiles and thus it need to move slightly less than 48 in)
     yTar *= fieldRatio;
     toPointLoops = 0;
     tInt = 0.0;
@@ -125,36 +144,36 @@ void toPoint(float xTar, float yTar, float reversed, bool smooth, float powerLim
     maxDist = sqrt(pow(xTar - xPos, 2.0) + pow(yTar - yPos, 2.0));
     while (toPointLoops < 50){
 
-        if(updateTargets){
+        if(updateTargets){ // Offsets targets if short-by is used
             xTar = xTarShortInput - offsetDist * cos(angleToTarget);
             yTar = yTarShortInput - offsetDist * sin(angleToTarget);
         }
 
-        tPos = (pi / 2.0) - tPos + pi * reversed; //! UN FRANK THIS BAD
-        tTarget = arctan2(xTar - xPos, yTar - yPos);
-        tError = normAngle(tTarget - tPos);
-        tInt += tError;
-        //if (fabs(tError) <= tErrorMin || fabs(tInt) >= tIntMax){tInt = 0.0;} //*tune rotKI first
-        tDer = tError - tPrevError;
-        tPrevError = tError;
-        tPow = TProtKP * tError + TProtKI * tInt + TProtKD * tDer;
+        tPos = (pi / 2.0) - tPos + pi * reversed;// Set "0 Degrees" -> "pi/2 Radians" for easier math (Sets heading opposite for reverse driving manuvers) //! Not exactly sure how this works 
+        tTarget = arctan2(xTar - xPos, yTar - yPos); // Calculate the radians to the target point
+        tError = normAngle(tTarget - tPos); // Find error and normalise to -pi to pi
+        tInt += tError; // Update integral
+        tDer = tError - tPrevError; // Update derivative
+        tPrevError = tError; // Update previous error
+        tPow = TProtKP * tError + TProtKI * tInt + TProtKD * tDer; // Calculate robots desired tuning power
 
-        dist = sqrt(pow(xTar - xPos, 2.0) + pow(yTar - yPos, 2.0));
-        lError = (1.0 - 2.0 * reversed) * dist * cos(tError);
-        lInt += lError;
-        if (fabs(lError) <= lErrorMin || fabs(lInt) >= lIntMax){lInt = 0.0;} //*tune linKI first
-        lDer = lError - lPrevError;
-        lPrevError = lError;
-        lPow = linKP * lError + linKI * lInt + linKD * lDer;
+        dist = sqrt(pow(xTar - xPos, 2.0) + pow(yTar - yPos, 2.0)); // Find distance to target
+        lError = (1.0 - 2.0 * reversed) * dist * cos(tError); // Find cos of the error (prioritize turning before going straight)
+        lInt += lError; // Update integral
+        if (fabs(lError) <= lErrorMin || fabs(lInt) >= lIntMax){lInt = 0.0;} // Limit integral power
+        lDer = lError - lPrevError; // Update derivative 
+        lPrevError = lError; // Update previous error
+        lPow = linKP * lError + linKI * lInt + linKD * lDer; // Calculate power desired for linear power
 
+        // Scale turning power based no distance to target (As you get close your delta theta can get really high as you are close so you need to limit it)
         tWeightA = (1.0 + sqrt(1.0 + 4.0 / tWeightK)) / 2.0;
         tWeightB = 1.0 / (tWeightK * (tWeightA - 1.0));
         tWeight = 1.0 / (tWeightK * (1.0 - (dist / maxDist) - tWeightA)) + tWeightB;
-        //if (dist < deadZoneRadius){tWeight = 0.0;}
 
-        rightPow = lPow + tWeight * tPow;
+        rightPow = lPow + tWeight * tPow; // Calculate right and left motor power
         leftPow = lPow - tWeight * tPow;
 
+        // Scale motor power if one is above motor input limit (If the robot wants to turn and go straight at full power the code will output 24,000mV on one side and 13,000mV on the other, the code is trying to turn but the robot will just go straight as 12,000mV is the max.
         if (fabs(rightPow) >= powerLimit || fabs(leftPow) >= powerLimit){
             if (fabs(rightPow) > fabs(leftPow)){
                 rightPow = getDir(lPow + tWeight * tPow) * powerLimit;
@@ -166,51 +185,25 @@ void toPoint(float xTar, float yTar, float reversed, bool smooth, float powerLim
             }
         }
 
-        //if(reversed == 1.0){rightPow *= -1.0; leftPow *= -1.0;}
-        //rightPow = 0;
-        //leftPow = 0;
-        rightDrive.move_voltage(rightPow);
+        rightDrive.move_voltage(rightPow); // Spin motors
         leftDrive.move_voltage(leftPow);
 
-        //lcd::set_text(4, std::to_string(tPos));
-        //lcd::set_text(6, std::to_string(rightPow));
-        //lcd::set_text(7, std::to_string(tError));
 
-        if(pseudoVelSwitch == 0 && pseudoVelocity > pseudoVelLimit){pseudoVelSwitch = 1;}
+        if(pseudoVelSwitch == 0 && pseudoVelocity > pseudoVelLimit){pseudoVelSwitch = 1;} // Prevents pseudo velocity from triggering right at start of the movement when the robot is not moving yet
 
-        /* //!i took this out between switching from positive to negative corner
-        if(smooth == 1 && exitDis > distance(xPos,yPos,xTar,yTar)){
-            toPointLoops += 100; //immediately exits loop
+        if (dist < distLimit || (pseudoVelSwitch == 1 && pseudoVelocity < pseudoVelLimit && checkPseudoVel == 1)){ // If the robot is within a certain amount from the target or the robot has stopped, start exiting the loop
+            if (smooth == 0){toPointLoops += 1;}
+            else{toPointLoops += 10;} // Exits earlier if smooth
         }
-        */
+        else{toPointLoops = 0;} // If doesn't meet any condition reset
 
-        if (dist < distLimit || (pseudoVelSwitch == 1 && pseudoVelocity < pseudoVelLimit && checkPseudoVel == 1)){
-            if (smooth == 0){toPointLoops += 1;} // +=1
-            else{toPointLoops += 10;} //+=10
-        }
-        else{toPointLoops = 0;}
-
-        /*
-        lcd::clear();
-        //lcd::print(0, "%f : rightPow", rightPow);
-        //lcd::print(1, "%f : leftPow", leftPow);
-        lcd::print(0, "%f : xTar", xTar);
-        lcd::print(1, "%f : yTar", yTar);
-        //lcd::print(2, "%f : tPos", tPos);
-        lcd::print(4, "%f : lError", lError);
-        lcd::print(5, "%f : lPow", lPow / 1000.0);
-        lcd::print(6, "%f : tError", tError);
-        lcd::print(7, "%f : tPow", tWeight * tPow / 1000.0);
-        */
-
-       //toPointLoops = 0;
         delay(10);
     }
     drivetrain.brake();
     updateTargets = 0;
 }
 
-void toPointShortBy(float xTar, float yTar, float reversed, bool smooth, float offsetDist, float powerLimit){
+void toPointShortBy(float xTar, float yTar, float reversed, bool smooth, float offsetDist, float powerLimit){ // Calculates toPoint path that is short by so many inches, example if you wanted to grab a goal in the corner you would put in the coordinates of the corner and then short by so many inches for where the robot should stop.
     float angleToTarget = arctan2(xTar - xPos, yTar - yPos);
     xTarShortInput = xTar;
     yTarShortInput = yTar;
@@ -219,11 +212,11 @@ void toPointShortBy(float xTar, float yTar, float reversed, bool smooth, float o
     toPoint(xTar - offsetDist * cos(angleToTarget), yTar - offsetDist * sin(angleToTarget), reversed, smooth, powerLimit);
 }
 
-void faceAway(float xTar, float yTar, bool fpSmooth, float speedLimit){
+void faceAway(float xTar, float yTar, bool fpSmooth, float speedLimit){ // Uses toPoint to face away from a point instead of at it
     facePoint(2.0 * xPos - xTar, 2.0 * yPos - yTar, fpSmooth, speedLimit);
 }
 
-void faceHeading(float tTar, bool fpSmooth, float speedLimit){
-    tTar *= pi / 180.0;
+void faceHeading(float tTar, bool fpSmooth, float speedLimit){ // Faces a certain global heading instead of point
+    tTar *= pi / 180.0; // Degrees to radians 
     facePoint(xPos + 96.0 * cos(tTar), yPos + 96.0 * sin(tTar), fpSmooth, speedLimit);
 }
